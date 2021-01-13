55a172c9ae212ee3dede44b6dec378c5ab901085
Janne <janne.majasalmi@gmail.com>
2021-01-13 11:35:26 +0200

    redux state is now passed to items component

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import Item from './Item/Item';
 import useStyles from './styles';
 
 const Items = () => {
-
+    const items = useSelector((state) =>state.items)
     const classes = useStyles();
+
+    console.log(items);
 
     return (
         <>
